e7b475138c89ac03800abb25ccfde8fcde594590
Ogtay Garayev <okaraev@outlook.com>
2022-07-01 22:38:56 +0400

    Fixed retrier

--- messageQ.go
+++ messageQ.go
@@ -13,61 +13,80 @@ import (
 )
 
 type Retrier struct {
-	Status    string        // Retrier Status
-	HoldTime  time.Duration // Time duration after which Operation will retry. If Operation fails hold time multiplies until 360 seconds
-	Operation func(q string, qq string) (<-chan amqp.Delivery, error)
+	Status      string        // Retrier Status
+	HoldTime    time.Duration // Time duration after which Operation will retry. If Operation fails hold time multiplies until 360 seconds
+	MaxHoldTime time.Duration
+	Operation   func(q string, qq string) (<-chan amqp.Delivery, error)
 }
 
 func (r *Retrier) New(myFunc func(q string, qq string) (<-chan amqp.Delivery, error)) {
 	r.HoldTime = 10 * time.Second
+	r.MaxHoldTime = 360 * time.Second
 	r.Status = "Closed"
 	r.Operation = myFunc
+}
+
+func (r *Retrier) Open() {
+	r.Status = "Open"
+}
+
+func (r *Retrier) Close() {
+	r.HoldTime = 10 * time.Second
+	r.Status = "Closed"
+}
+
+func (r *Retrier) Multiply() {
+	if r.HoldTime <= r.MaxHoldTime {
+		r.HoldTime = r.HoldTime * 2
+	}
+}
+
+func (r *Retrier) isClosed() bool {
+	return r.Status == "Closed"
 }
 
 func (r *Retrier) Do(RMQServer string, QName string) (<-chan amqp.Delivery, error) {
 	result, err := r.Operation(RMQServer, QName)
 	mydelivery := make(chan amqp.Delivery)
 	if err != nil {
-		if r.HoldTime <= 360*time.Second {
-			r.HoldTime = r.HoldTime * 2
-			r.Status = "Open"
-			log.Println(err)
-		}
+		r.Open()
+		log.Println(err)
 	}
-	if r.Status == "Closed" {
+	if r.isClosed() {
+		// if status is success starting routine to receive messages
 		go func() {
 			for message := range result {
 				mydelivery <- message
 			}
+			// Ending routine when error happened and setting status to fail
+			r.Open()
 		}()
 	}
+	// Starting forever loop in routine to check if status is fail
 	go func() {
 		for {
 			time.Sleep(1 * time.Second)
-			if r.Status == "Open" {
+			if !r.isClosed() {
+				// if status is fail waiting for defined hold time and trying again
 				time.Sleep(r.HoldTime)
 				retryResult, err := r.Operation(RMQServer, QName)
 				if err != nil {
-					log.Println(err)
-					if r.HoldTime*2 <= 360*time.Second {
-						r.HoldTime = r.HoldTime * 2
-					}
+					r.Multiply()
 				} else {
-					r.Status = "Closed"
+					// if retry is success setting status to success and starting routine to receive messages
+					r.Close()
 					go func() {
 						for message := range retryResult {
 							mydelivery <- message
 						}
+						// Ending routine when error happened and setting status to fail
+						r.Open()
 					}()
 				}
 			}
 		}
 	}()
 	return (<-chan amqp.Delivery)(mydelivery), nil
-}
-
-func (r *Retrier) Open() {
-	r.Status = "Open"
 }
 
 func ReceiveMessage(RMQServer string, queue string) (<-chan amqp.Delivery, error) {
@@ -138,32 +157,6 @@ func DelayMessage(RMQServer string, QName string, i interface{}) error {
 		Body:        bytes,
 	}
 	err = channelRabbitMQ.Publish("", DelayQ, false, false, message)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func SendMessage(amqpServerURL string, QName string, i interface{}) error {
-	connectRabbitMQ, err := amqp.Dial(amqpServerURL)
-	if err != nil {
-		return err
-	}
-	defer connectRabbitMQ.Close()
-	channelRabbitMQ, err := connectRabbitMQ.Channel()
-	if err != nil {
-		return err
-	}
-	defer channelRabbitMQ.Close()
-	bytes, err := json.Marshal(i)
-	if err != nil {
-		return err
-	}
-	message := amqp.Publishing{
-		ContentType: "application/json",
-		Body:        bytes,
-	}
-	err = channelRabbitMQ.Publish("", QName, false, false, message)
 	if err != nil {
 		return err
 	}
